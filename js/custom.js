6b51fdf2e61b54a3d639d4696767afbe2e215385
Rohan Singh <rohansingh@Rohans-MacBook-Air.local>
2021-01-30 20:52:00 +0530

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,85 +1,13 @@
 
-
-$(window).scroll(function () {
-
-  if ($(window).scrollTop() >= 1) {
-      $('.header').addClass('fixedHeader');
-  } else {
-      $('.header').removeClass('fixedHeader');
-  }
+  $(window).scroll(function () {
+if ($(window).scrollTop() >= 1) {
+    $('.header').addClass('fixedHeader');
+} else {
+    $('.header').removeClass('fixedHeader');
+}
 });
 
-$(".switch.switch1").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll1").offset().top
-  }, 1000);
-});
-$(".switch.switch2").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll2").offset().top
-  }, 1000);
-});
-$(".switch.switch3").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll3").offset().top
-  }, 1000);
-});
-$(".switch.switch4").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll4").offset().top
-  }, 1000);
-});
-$(".switch.switch5").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll5").offset().top
-  }, 1000);
-});
-$(".switch.switch6").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll6").offset().top
-  }, 1000);
-});
-$(".switch.switch7").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scroll7").offset().top
-  }, 1000);
-});
 
-$(".switch.switchh1").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scrolltop2").offset().top
-  }, 1000);
-});
-$(".switch.switchh2").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scrolltop3").offset().top
-  }, 1000);
-});
-$(".switch.switchh3").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scrolltop4").offset().top
-  }, 1000);
-});
-$(".switch.switchh4").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scrolltop5").offset().top
-  }, 1000);
-});
-$(".switch.switchh5").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scrolltop6").offset().top
-  }, 1000);
-});
-$(".switch.switchh6").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".scrolltop1").offset().top
-  }, 1000);
-});
-$(".switch.switchf1").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".footscroll").offset().top
-  }, 1000);
-});
 jQuery("#carousel").owlCarousel({
     autoplay: false,
     lazyLoad: true,
@@ -178,8 +106,6 @@ $('#qwe').click(function() {
   $('#file-name').attr("value", "");  
   // $('#ht1').attr("value", "");  
 });
-$(document).on('scroll',function(){
-  $('#file-name').attr("value", " "); 
-});
+
 
 
